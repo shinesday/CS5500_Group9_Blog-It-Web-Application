54519d875dd612314b8574e8eeb3a7228ad3e349
qingxuanma <ma.qingx@northeastern.edu>
2022-10-21 11:30:22 -0700

    create api

--- blog-front-end/build/webpack.prod.conf.js
+++ blog-front-end/build/webpack.prod.conf.js
@@ -57,9 +57,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         ? { safe: true, map: { inline: false } }
         : { safe: true }
     }),
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
+
     new HtmlWebpackPlugin({
       filename: config.build.index,
       template: 'index.html',
