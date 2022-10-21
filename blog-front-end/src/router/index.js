2773c2ce829369cfba9b1f8ddd3a3460181e982f
Shine Shen <shinesday@gmail.com>
2022-10-21 13:41:48 -0700

    update config

--- blog-front-end/src/router/index.js
+++ blog-front-end/src/router/index.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/Home'
-/*import Index from '@/views/Index'
-import Login from '@/views/Login'
-import Register from '@/views/Register'
-import Log from '@/views/Log'
-import MessageBoard from '@/views/MessageBoard'
-import BlogWrite from '@/views/blog/BlogWrite'
-import BlogView from '@/views/blog/BlogView'
-import BlogAllCategoryTag from '@/views/blog/BlogAllCategoryTag'
-import BlogCategoryTag from '@/views/blog/BlogCategoryTag'*/
 
 import {Message} from 'element-ui';
 
