54eac60bb0e67f78a2cb911bfd075a12bdd830ec
Shine Shen <shinesday@gmail.com>
2022-10-21 16:13:41 -0700

    update view configs

---
+++ blog-front-end/src/components/card/CardCategory.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <el-card :body-style="{ padding: '8px 18px' }">
+    <div slot="header" class="me-category-header">
+      <span>Newest Articles</span>
+    </div>
+
+    <ul class="me-category-list">
+      <li class="me-category-item" v-for="l in 3" :key="l"><a>Title 1 Title 1</a></li>
+      <li class="me-category-item" v-for="l in 3" :key="l"><a>Built element-ui using Vue as front-end</a></li>
+
+    </ul>
+  </el-card>
+
+</template>
+
+<script>
+  export default {
+    name: 'CardCategory',
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+  .me-category-header {
+    font-weight: 600;
+  }
+
+  .me-category-list {
+    list-style-type: none;
+  }
+
+  .me-category-item {
+    padding: 4px;
+    font-size: 14px;
+    color: #5FB878;
+  }
+
+  .me-category-item a:hover {
+    text-decoration: underline;
+  }
+</style>
--- blog-front-end/src/main.js
+++ blog-front-end/src/main.js
@@ -1,6 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 
-import './assets/main.css'
+import Vue from 'vue'
+import App from './App'
 
-createApp(App).mount('#app')
+import router from './router'
+import store from './store'
+
+import lodash from 'lodash'
+
+import ElementUI from 'element-ui'
+import '@/assets/theme/index.css'
+
+import '@/assets/icon/iconfont.css'
+
+import {formatTime} from "./utils/time";
+
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+Object.defineProperty(Vue.prototype, '$_', { value: lodash })
+
+
+Vue.directive('title',  function (el, binding) {
+  document.title = el.dataset.title
+})
+// Formatting time
+Vue.filter('format', formatTime)
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
